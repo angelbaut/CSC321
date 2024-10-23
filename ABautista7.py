ff47dc8f6faf44066122fed95d04ad638d3e6c0d
ABautista <abautista59@toromail.csudh.edu>
2024-10-23 01:30:35 +0000

    Lab 7

---
+++ ABautista7.py
@@ -0,0 +1,22 @@
+#Angel Bautista 
+
+
+x = 13
+y= 77.2
+z= "Georgia"
+
+if x < 6 :
+    print("x is less than 7")
+else:
+    print("x is not less than 7")
+
+if y > 18.3:
+    print("y is grater than 18.3")
+else:
+    print("y is not grater than 18.3")
+
+if z == "Georgia":
+    print("Variable z is Georgia")
+else:
+    print("Variable x is not Georgia")
+
